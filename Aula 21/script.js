271b0ec29ca13fb03aacf11d192c75da9d540b0b
Daniel Santana <62359747+DanielASantana777@users.noreply.github.com>
2024-05-29 22:35:50 -0300

    Add files via upload

---
+++ Aula 21/script.js
@@ -0,0 +1,8 @@
+function valor(){
+    var dia = window.document.getElementById("dia")
+    var Mes = window.document.getElementById("mes")
+    var Ano = window.document.getElementById("ano")
+
+    window.alert(dia)
+
+}
